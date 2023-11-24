4a4f4ec520e801992ea0fb39c9fa1e35d850fcad
Patrik Koczka <patrik.koczka@incquerylabs.com>
2023-11-24 03:18:04 +0100

    #Szakdolgozat Pattern and rule

--- hu.bme.mit.sysml2autosar.transformation/src/hu/bme/mit/sysml2autosar/transformation/BatchModelTransformation.java
+++ hu.bme.mit.sysml2autosar.transformation/src/hu/bme/mit/sysml2autosar/transformation/BatchModelTransformation.java
@@ -2,20 +2,29 @@ package hu.bme.mit.sysml2autosar.transformation;
 
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.uml2.uml.Interface;
+import org.eclipse.uml2.uml.Property;
 import org.eclipse.viatra.query.runtime.api.ViatraQueryEngine;
 import org.eclipse.viatra.query.runtime.emf.EMFScope;
 import org.eclipse.viatra.transformation.runtime.emf.modelmanipulation.IModelManipulations;
 import org.eclipse.viatra.transformation.runtime.emf.modelmanipulation.SimpleModelManipulations;
 import org.eclipse.viatra.transformation.runtime.emf.rules.batch.BatchTransformationRule;
 import org.eclipse.viatra.transformation.runtime.emf.rules.batch.BatchTransformationRuleFactory;
+import org.eclipse.viatra.transformation.runtime.emf.transformation.batch.BatchTransformation;
 import org.eclipse.viatra.transformation.runtime.emf.transformation.batch.BatchTransformationStatements;
 
 import autosar40.autosartoplevelstructure.AUTOSAR;
 import autosar40.autosartoplevelstructure.AutosartoplevelstructureFactory;
 import autosar40.genericstructure.generaltemplateclasses.arpackage.ARPackage;
 import autosar40.genericstructure.generaltemplateclasses.arpackage.ArpackageFactory;
-
-import org.eclipse.viatra.transformation.runtime.emf.transformation.batch.BatchTransformation;
+import autosar40.genericstructure.generaltemplateclasses.arpackage.PackageableElement;
+import autosar40.swcomponent.portinterface.ClientServerInterface;
+import autosar40.swcomponent.portinterface.SenderReceiverInterface;
+import autosar40.swcomponent.portinterface.PortinterfaceFactory;
+import hu.bme.mit.sysml2autosar.queries.AtomicSwComponentTypes;
+import hu.bme.mit.sysml2autosar.queries.ClientServerInterfaceType;
+import hu.bme.mit.sysml2autosar.queries.CompositionSwComponentTypes;
+import hu.bme.mit.sysml2autosar.queries.SenderReceiverInterfaceType;
 
 public class BatchModelTransformation {
 
@@ -88,28 +97,36 @@ public class BatchModelTransformation {
 		statements = transformation.getTransformationStatements();
 	}
 	
-	private BatchTransformationRule<?, ?> getClientServerInterfaceRule() {
-		//if(clientServerInterfaceRule == null) {
-			//clientServerInterfaceRule = createRule.name("ClientServerInterfaceRule")
-				//	ClientServerInterfaceMatcher.querySpecification
-			//createRule(ClientServerInterface.instance()).action(match -> {
-	        //}).build());
-//			clientServerInterfaceRule = createRule.name("ClientServerInterfaceRule").
-		//precondition(ExampleMatcher.querySpecification).action[]
-		//}
-		return clientServerInterfaceRule;
+	private void getClientServerInterfaceRule() {
+		batchTransformationRuleFactory.createRule(ClientServerInterfaceType.instance()).name("ClientServerInterfaceRule").action(match -> {
+			Interface csi = match.getUmlInterface();
+			String csi_name = csi.getName();
+			ClientServerInterface autosarInterface = PortinterfaceFactory.eINSTANCE.createClientServerInterface();
+			autosarInterface.setShortName(csi_name);
+			portInterfaces.getElements().add(autosarInterface);
+		}).build();
 	}
 	
 	private void getSenderReceiverInterfaceRule() {
-		
+		batchTransformationRuleFactory.createRule(SenderReceiverInterfaceType.instance()).name("SenderReceiverInterfaceRule").action(match -> {
+			Interface sri = match.getUmlInterface();
+			String sri_name = sri.getName();
+			SenderReceiverInterface autosarInterface = PortinterfaceFactory.eINSTANCE.createSenderReceiverInterface();
+			autosarInterface.setShortName(sri_name);
+			portInterfaces.getElements().add(autosarInterface);
+		}).build();
 	}
 	
 	private void getAtomicSwComponentTypesRule() {
-		
+		batchTransformationRuleFactory.createRule(AtomicSwComponentTypes.instance()).name("AtomicSwComponentTypesRule").action(match -> {
+			Property aswct = match.getUmlProperty();
+		}).build();
 	}
 	
 	private void getCompositionSwComponentTypesRule() {
-		
+		batchTransformationRuleFactory.createRule(CompositionSwComponentTypes.instance()).name("CompositionSwComponentTypesRule").action(match -> {
+			Property cswct = match.getUmlProperty();
+		}).build();
 	}
 
 //  private def getExampleRule() {
